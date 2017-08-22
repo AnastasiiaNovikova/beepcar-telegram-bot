9088f797cf81b3cc8fec2e5cf17df7a7ce50ecdf
Denis Isaev <d.isaev@corp.mail.ru>
2017-08-22 21:58:29 +0300

    wip

--- main.go
+++ main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// TelegramSender ...
+type TelegramSender struct {
+	ID        int
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+// TelegramWebHookPayload is a payload in WebHook message from Telegram API
+type TelegramWebHookPayload struct {
+	UpdateID int `json:"update_id"`
+	Message  struct {
+		Date      uint32
+		Text      string
+		MessageID string `json:"message_id"`
+		Chat      struct {
+			TelegramSender
+			Type string
+		}
+		From struct {
+			TelegramSender
+			LanguageCode string `json:"language_code"`
+		}
+	}
+}
+
 func handleUpdateWebHook(r *http.Request) error {
-	defer r.Body.Close()
-	reqBody, err := ioutil.ReadAll(r.Body)
+	var payload TelegramWebHookPayload
+	err := json.NewDecoder(r.Body).Decode(payload)
 	if err != nil {
-		return fmt.Errorf("can't read request body: %s", err)
+		return fmt.Errorf("invalid JSON in webhook: %s", err)
 	}
 
-	fmt.Printf("request body is %q", reqBody)
+	fmt.Printf("webhook payload is %v", payload)
 	return nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 	err := handleUpdateWebHook(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -29,6 +56,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var listenAddr string
+	flag.StringVar(&listenAddr, "listen-addr", "127.0.0.1:3030", "address to listen")
+	flag.Parse()
+
 	http.HandleFunc("/btapi/update", handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
